e30defb9dd88c1a60dc48a9a141db9b798b6bf4e
MOHIT KRISHNIA <98814388+Mohit-krishnia@users.noreply.github.com>
2023-03-26 19:32:57 +0530

    Update find the way with map.py

--- assignment4/find the way with map.py
+++ assignment4/find the way with map.py
@@ -1,8 +1,4 @@
-a=int(input("how much element you want to enter in list : "))
-l=[]
-for j in range(1,a+1):
-      j=int(input())
-      l.append(j)
+a=map(int,input("Enter the string elements : ").split())
 print("entered list is : ",l)
 c=list(map(lambda i: i*3 ,l))
 print("output of list  : ",c)
