cf435f178cc9b823589b7527da1511efd55513ba
MOHIT KRISHNIA <98814388+Mohit-krishnia@users.noreply.github.com>
2023-03-26 19:34:55 +0530

    Update find the way with map.py

--- assignment4/find the way with map.py
+++ assignment4/find the way with map.py
@@ -1,4 +1,4 @@
-a=map(int,input("Enter the string elements : ").split())
+l=map(int,input("Enter the string elements : ").split())
 print("entered list is : ",l)
 c=list(map(lambda i: i*3 ,l))
 print("output of list  : ",c)
